7934200b8ddb064d4ab6906d07999f06d5eadae9
Juan Arillo <juanarillo@outlook.com>
2016-07-05 20:23:14 +0200

    added Tag class

---
+++ hackerbooks/hackerbooks/Tag.swift
@@ -0,0 +1,70 @@
+//
+//  Tag.swift
+//  hackerbooks
+//
+//  Created by Juan Arillo Ruiz on 5/7/16.
+//  Copyright © 2016 Juan Arillo Ruiz. All rights reserved.
+//
+
+import Foundation
+
+class Tag : Hashable, Comparable {
+    
+    // MARK: - Properties
+    private var _name: String? = nil
+    var name : String {
+        set {
+            self._name = newValue
+        }
+        get {
+            if let name = _name{
+                return name.lowercaseString.capitalizedString
+            }
+            
+            return ""
+        }
+    }
+    
+    var hashValue: Int{
+        
+        get {
+            return name.hashValue
+        }
+        
+    }
+    
+    // MARK: - Initializers
+    
+    init(name: String){
+        self.name = name
+    }
+    
+    // MARK: - Proxies
+    
+    var proxyForSorting : String{
+        get {
+            return "A\(name)"
+        }
+    }
+}
+
+// MARK: - Equatable & Comparable
+
+func == (lhs: Tag, rhs: Tag) -> Bool {
+    return lhs.hashValue == rhs.hashValue
+}
+
+func <(lhs: Tag, rhs: Tag) -> Bool {
+    return (lhs.proxyForSorting < rhs.proxyForSorting)
+}
+
+// MARK: - Extensions
+
+extension Tag : CustomStringConvertible {
+    
+    var description : String{
+        get {
+            return "<\(self.dynamicType): \(name)>"
+        }
+    }
+}
